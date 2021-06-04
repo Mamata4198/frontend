d05a322931998e1f2dc8af56b64b665ad601220a
Mamata4198 <mamata.bb@terralogic.com>
2021-06-04 13:25:03 +0530

    update1

--- src/app/components/account/account-details/account-details.component.ts
+++ src/app/components/account/account-details/account-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountDetailsApiService } from 'src/app/service/account-details-api.service';
+import { GridOptions } from 'ag-grid-community';
 
 @Component({
   selector: 'app-account-details',
@@ -8,65 +9,87 @@ import { AccountDetailsApiService } from 'src/app/service/account-details-api.se
 })
 export class AccountDetailsComponent implements OnInit {
   [x: string]: any;
+  gridOptions: GridOptions;
+  balance = 30000;
+  prevBal = this.balance;
+
   columnDefs = [
     {
-        headerName: "slNo",
-        field: "slNo",
-        width: 70,
-        sortable: true ,
-        filter: true,  
-        
+      headerName: "slNo",
+      field: "slNo",
+      width: 70,
+      sortable: true,
+      filter: true,
+
     },
     {
-        headerName: "transactionNo",
-        field: "transactionNo",
-        width: 150,
-        sortable: true ,
-        filter: true,  
-        
+      headerName: "transactionNo",
+      field: "transactionNo",
+      width: 150,
+      sortable: true,
+      filter: true,
+
     },
     {
       headerName: "transaction",
       field: "transaction",
       width: 150,
-      sortable: true ,
-      filter: true,  
-     
-  },
-  {
-    headerName: "credit",
-    field: "credit",
-    width: 100,
-    sortable: true ,
-    filter: true,  
+      sortable: true,
+      filter: true,
 
-},
-{
-  headerName: "debit",
-  field: "debit",
-  width: 100,
-  sortable: true ,
-  filter: true,  
+    },
+    {
+      headerName: "credit",
+      field: "credit",
+      width: 100,
+      sortable: true,
+      filter: true,
 
-},
-{
-  headerName: "total",
-  field: " ",
-  width: 100,
-  sortable: true ,
-  filter: true,  
+    },
+    {
+      headerName: "debit",
+      field: "debit",
+      width: 100,
+      sortable: true,
+      filter: true,
 
-}
+    },
+    {
+      headerName: "total",
+      field: "previousBal",
+      width: 100,
+      sortable: true,
+      filter: true,
 
 
+    }
 
-];
-   rowData : any;
+
+
+  ];
+  rowData: any;
 
   constructor(private accountDetailsApiService: AccountDetailsApiService) { }
 
   ngOnInit() {
-    this.rowData = this.accountDetailsApiService. getAccountsDetails();
+   this.getAccountsDetails();
+    // this.rowData = this.accountDetailsApiService. getAccountsDetails();
+    // this.rowData[this.total]=this.rowData;
   }
+  getAccountsDetails(): void {
+    this.accountDetailsApiService.getAccountsDetails().subscribe((res: any) => {
+     res= res.sort((a,b) => (+a.slNo < +b.slNo) ? 1 : ((+b.slNo < +a.slNo) ? -1 : 0) );
+      let prevBal = this.balance;
+      let prevAggregatorValue = 0;
+     res = res.map((row) => {
+        prevBal = prevBal + prevAggregatorValue;
+        row.previousBal = prevBal;
+        prevAggregatorValue = (-row.credit) + (+row.debit);
+        return row;
+      });
+      this.rowData= res.sort((a,b) => (+a.slNo > +b.slNo) ? 1 : ((+b.slNo > +a.slNo) ? -1 : 0) );
+    });
+}
 
 }
+
--- src/app/components/account/accounts/accounts.component.ts
+++ src/app/components/account/accounts/accounts.component.ts
@@ -46,8 +46,6 @@ export class AccountsComponent implements OnInit {
      })    
 
 }
-
-
 mainForm() {
   this.accountForm = this.fb.group({
     name: ['',[Validators.required,Validators.minLength(3)]],
